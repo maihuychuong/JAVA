58c6b0afad821ba6a6bb3bb49c521cc222039383
maihuychuong <maihuychuong78@gmail.com>
2024-09-12 16:40:53 +0700

    1

---
+++ Kiem tra opp 2/src/midterm/service/UserService.java
@@ -0,0 +1,138 @@
+package midterm.service;
+
+
+import midterm.data.Database;
+import midterm.entities.User;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserService {
+
+    public void register(Scanner scanner){
+        System.out.println("Nhập username: ");
+        String username = scanner.nextLine();
+        System.out.println("Nhập email: ");
+        String email = scanner.nextLine();
+        System.out.println("Nhập password: ");
+        String password = scanner.nextLine();
+        if (!conditionEmail(email)) {
+            System.out.println("Email không hợp lệ. Vui lòng nhập email đúng định dạng.");
+            return;
+        }
+        if (!conditionPassword(password)) {
+            System.out.println("Mật khẩu không hợp lệ. Mật khẩu phải chứa ít nhất 1 ký tự viết hoa, 1 ký tự đặc biệt và có độ dài từ 7 đến 15 ký tự.");
+            return;
+        }
+        if(findUserByUsernameAndEmail(username, email) == null){
+            Database.users.add(new User(username, password, email));
+            System.out.println("Đăng lý thành công");
+        } else {
+            System.out.println("Username hoặc email đã tồn tại ");
+        }
+    }
+
+    public User login(Scanner scanner){
+        System.out.println("Nhập username: ");
+        String username = scanner.nextLine();
+        System.out.println("Nhập password: ");
+        String password = scanner.nextLine();
+        User user = findUserByUsernameAndPassword(username, password);
+        if(user != null){
+            return user;
+        } else {
+            return null;
+        }
+    }
+
+    private boolean conditionEmail(String email) {
+        String emailRegex = "^[\\w-\\.]+@[\\w-]+\\.[a-z]{2,4}$";
+        return Pattern.matches(emailRegex, email);
+    }
+
+    private boolean conditionPassword(String password) {
+        String passwordRegex = "^(?=.*[A-Z])(?=.*[!@#$&*.-_])[A-Za-z0-9!@#$&*.-_]{7,15}$";
+        return Pattern.matches(passwordRegex, password);
+    }
+
+    private User findUserByUsername(String username){
+        for (User user: Database.users){
+            if(user.getUsername().equals(username)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    private User findUserByEmail(String email){
+        for (User user: Database.users){
+            if(user.getEmail().equals(email)){
+                return user;
+            }
+        }
+        return null;
+    }
+    private User findUserByUsernameAndPassword(String username, String password){// 100
+        for (User user: Database.users){
+            if(user.getUsername().equals(username) && user.getPassword().equals(password)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    private User findUserByUsernameAndEmail(String username, String email){
+        for (User user: Database.users){
+            if(user.getUsername().equals(username) && user.getEmail().equals(email)){
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public void changeUsername(Scanner scanner, User user){
+        System.out.println("Nhập username mới: ");
+        String username = scanner.nextLine();
+        while (findUserByUsername(username) != null) {
+            System.out.println("Username đã tồn tại. Nhập lại username mới: ");
+            username = scanner.nextLine();
+        }
+        user.setUsername(username);
+        System.out.println("Username đã được thay đổi thành công.");
+    }
+
+    public void changeEmail(Scanner scanner, User user) {
+        System.out.println("Nhập email mới: ");
+        String email = scanner.nextLine();
+        while (!conditionEmail(email) || findUserByEmail(email) != null) {
+            if (!conditionEmail(email)) {
+                System.out.println("Email không hợp lệ. Nhập lại email mới: ");
+            } else {
+                System.out.println("Email đã tồn tại. Nhập lại email mới: ");
+            }
+            email = scanner.nextLine();
+        }
+        user.setEmail(email);
+    }
+
+    public void changePassword(Scanner scanner, User user){
+        System.out.println("Nhập password mới: ");
+        String password = scanner.nextLine();
+        while (!conditionPassword(password)){
+            System.out.println("Password không hợp lệ. Nhập lại password mới: ");
+            password = scanner.nextLine();
+        }
+        user.setPassword(password);
+    }
+    public void resetPassword(Scanner scanner){
+        System.out.println("Nhập email của tài khoản cần reset mật khẩu: ");
+        String email = scanner.nextLine();
+        User user = findUserByEmail(email);
+        while (user == null) {
+            System.out.println("Email không tồn tại. Nhập lại email: ");
+            email = scanner.nextLine();
+            user = findUserByEmail(email);
+        }
+        changePassword(scanner, user);
+    }
+}
---
+++ Kiem tra opp 2/src/midterm/view/Menu.java
@@ -0,0 +1,98 @@
+package midterm.view;
+
+import midterm.entities.User;
+import midterm.service.UserService;
+
+import java.util.Scanner;
+
+public class Menu {
+    UserService userService = new UserService();
+
+    public void displayPreLogin(Scanner scanner){
+        System.out.println("1 - Đăng nhập");
+        System.out.println("2 - Đăng ký");
+        System.out.println("3 - Thoát chương trình");
+        System.out.println("Mời lựa chọn: ");
+        selectPreLogin(scanner);
+    }
+    private void selectPreLogin(Scanner scanner){
+        int choice = Integer.parseInt(scanner.nextLine());
+        switch (choice){
+            case 1:
+                System.out.println("Thực hiện login");
+                User user = userService.login(scanner);
+                if(user != null){
+                    System.out.println("Chào mừng "+user.getUsername()+" , bạn có thể thực hiện các công việc sau:”\\n");
+                    displayPostLogin(scanner, user);
+                } else {
+                    System.out.println("Username hoặc password không đúng");
+                    displayForgotPassword(scanner);
+                }
+                break;
+            case 2:
+                System.out.println("Thực hiện đăng ký");
+                userService.register(scanner);
+                displayPreLogin(scanner);
+                break;
+            case 3:
+                System.exit(0);
+        }
+    }
+
+    private void displayPostLogin(Scanner scanner, User user){
+        System.out.println("1 - Thay đổi username");
+        System.out.println("2 - Thay đổi email");
+        System.out.println("3 - Thay đổi password");
+        System.out.println("4 - Đăng xuất");
+        System.out.println("5 - Thoát chương trình");
+        System.out.println("Mời lựa chọn: ");
+        selectPostLogin(scanner, user);
+    }
+
+    private void selectPostLogin(Scanner scanner, User user){
+        int choice = Integer.parseInt(scanner.nextLine());
+        switch (choice){
+            case 1:
+                System.out.println("Thực hiện thay đổi username: ");
+                userService.changeUsername(scanner, user);
+                System.out.println("Username đã được thay đổi thành công.");
+                break;
+            case 2:
+                System.out.println("Thực hiện thay đổi email: ");
+                userService.changeEmail(scanner, user);
+                System.out.println("Email đã được thay đổi thành công.");
+                break;
+            case 3:
+                System.out.println("Thực hiện thay đổi password: ");
+                userService.changePassword(scanner, user);
+                System.out.println("Password đã được thay đổi thành công.");
+                break;
+            case 4:
+                System.out.println("Đăng xuất thành công.");
+                displayPreLogin(scanner);
+                break;
+            case 5:
+                System.exit(0);
+        }
+    }
+    public void displayForgotPassword(Scanner scanner){
+        System.out.println("1 - Đăng nhập lại.");
+        System.out.println("2 - Quên mật khẩu");
+        System.out.println("Mời lựa chọn: ");
+        sselectForgotPassword(scanner);
+    }
+    private void sselectForgotPassword(Scanner scanner){
+        int choice = Integer.parseInt(scanner.nextLine());
+        switch (choice){
+            case 1:
+                System.out.println("Vui lòng đăng nhập lại: ");
+                userService.login(scanner);
+                break;
+            case 2:
+                System.out.println("Thực hiện reset password: ");
+                userService.resetPassword(scanner);
+                System.out.println("Password đã được thay đổi thành công. Vui lòng đăng nhập lại: ");
+                userService.login(scanner);
+        }
+    }
+}
